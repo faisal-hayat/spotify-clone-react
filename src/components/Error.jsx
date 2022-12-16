22076b1bec460120d8d85b3816d2f95ac167df1b
M Faisal Hayat <m.faisalhayat3@gmail.com>
2022-12-17 01:31:49 +0500

    loading and error component done

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Error(props) {
   return (
-    <div>Error</div>
+    <div className='w-full flex justify-center items-center '>
+     <h1 className='font-bold text-2xl text-white mt-2'>
+        {props.title || "Failed to loadin songs..."}
+      </h1>
+    </div>
   )
 }
 
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { loader } from "../assets";
 
 function Loader(props) {
   return (
-    <div>Loader</div>
+    <div className='w-full flex justify-center items-center flex-col'>
+      <img src={loader} alt="loading images" className='w-32 h-32 object-contain' />
+      <h1 className='font-bold text-2xl text-white mt-2'>
+        Something went wrong, Please try again
+      </h1>
+    </div>
   )
 }
 
